4a0c136cdcd8fcaa9b4cec5251093bed749b7884
matteo <matteo.massimello@gmail.com>
2025-03-18 15:38:14 +0100

    feat: done the reading in a file for 8 byte

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ch := getLinesChannel(file)
+
+	for ln := range ch {
+		fmt.Printf("read: %s", ln)
+	}
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	prov := make(chan string)
+
+	go func() {
+		var byt []byte
+		byt = make([]byte, 8)
+		var current string
+		for {
+			n, err := f.Read(byt)
+			if err != nil || n == 0 {
+				if current != "" {
+					prov <- current
+					current = ""
+				}
+				break
+			}
+
+			str := string(byt[:n])
+			part := strings.Split(str, "\n")
+
+			for i := 0; i < len(part)-1; i++ {
+				prov <- current + part[i]
+				current = ""
+			}
+
+			current += part[len(part)-1]
+		}
+
+		close(prov)
+	}()
+
+	return prov
 }
