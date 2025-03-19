1cfd7c48af39d9829fda7086dfd786b018423ee9
matteo <matteo.massimello@gmail.com>
2025-03-19 11:28:28 +0100

    feat: refactor a little

--- internal/request/request.go
+++ internal/request/request.go
@@ -24,19 +24,31 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	var request Request
 	req := strings.Split(string(read), "\r\n")
 
-	var subdivision = strings.Split(req[0], " ")
-	if subdivision[0] != strings.ToUpper(subdivision[0]) {
-		return nil, errors.New("Method not valid")
+	request.RequestLine, err = requestLine(req[0])
+	if err != nil {
+		return nil, err
 	}
-	request.RequestLine.Method = subdivision[0]
-
-	if subdivision[len(subdivision)-1] != "HTTP/1.1" {
-		return nil, errors.New("Version of http not valid, only 1.1 can be used")
-	}
-
-	request.RequestLine.HttpVersion = strings.Split(subdivision[len(subdivision)-1], "/")[1]
-
-	request.RequestLine.RequestTarget = subdivision[1]
 
 	return &request, nil
+}
+
+func requestLine(req string) (RequestLine, error) {
+	var subdivision = strings.Split(req, " ")
+
+	if subdivision[0] != strings.ToUpper(subdivision[0]) {
+		return RequestLine{}, errors.New("Method not valid")
+	}
+
+	var resp RequestLine
+	resp.Method = subdivision[0]
+
+	if subdivision[len(subdivision)-1] != "HTTP/1.1" {
+		return RequestLine{}, errors.New("Version of http not valid, only 1.1 can be used")
+	}
+
+	resp.HttpVersion = strings.Split(subdivision[len(subdivision)-1], "/")[1]
+
+	resp.RequestTarget = subdivision[1]
+
+	return resp, nil
 }
