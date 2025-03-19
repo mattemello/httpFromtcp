ee659dae706bf3239ed3fbd2ac3397a06b25f5de
matteo <matteo.massimello@gmail.com>
2025-03-19 23:07:32 +0100

    feat: refactoring everything

--- internal/request/request.go
+++ internal/request/request.go
@@ -2,12 +2,23 @@ package request
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"strings"
 )
 
+var buffSize = 8
+
+type status int
+
+const (
+	intialized status = iota + 1
+	done
+)
+
 type Request struct {
 	RequestLine RequestLine
+	status      status
 }
 
 type RequestLine struct {
@@ -17,38 +28,89 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	read, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
 	var request Request
-	req := strings.Split(string(read), "\r\n")
+	request.status = intialized
 
-	request.RequestLine, err = requestLine(req[0])
-	if err != nil {
-		return nil, err
+	var buf = make([]byte, buffSize, buffSize)
+
+	var readIndex = 0
+
+	for request.status != done {
+
+		dim, err := reader.Read(buf[readIndex:])
+		if err != nil {
+			if err != io.EOF {
+				return nil, err
+			}
+			request.status = done
+		}
+
+		_, err = request.parse(buf)
+		if err != nil {
+			return nil, err
+		}
+
+		readIndex += dim
+
+		if dim == buffSize {
+			buffSize *= 2
+			newBuf := make([]byte, buffSize, buffSize)
+			copy(newBuf, buf)
+			buf = newBuf
+		}
+
+		fmt.Println(request.status)
+
 	}
 
 	return &request, nil
 }
 
-func requestLine(req string) (RequestLine, error) {
+func (r *RequestLine) parseRequestLine(req string) (int, error) {
+
 	var subdivision = strings.Split(req, " ")
 
 	if subdivision[0] != strings.ToUpper(subdivision[0]) {
-		return RequestLine{}, errors.New("Method not valid")
+		return 0, errors.New("Method not valid")
 	}
 
-	var resp RequestLine
-	resp.Method = subdivision[0]
+	r.Method = subdivision[0]
 
 	if subdivision[len(subdivision)-1] != "HTTP/1.1" {
-		return RequestLine{}, errors.New("Version of http not valid, only 1.1 can be used")
+		return 0, errors.New("Version of http not valid, only 1.1 can be used")
 	}
 
-	resp.HttpVersion = strings.Split(subdivision[len(subdivision)-1], "/")[1]
+	r.HttpVersion = strings.Split(subdivision[len(subdivision)-1], "/")[1]
 
-	resp.RequestTarget = subdivision[1]
+	r.RequestTarget = subdivision[1]
 
-	return resp, nil
+	return len([]byte(req)), nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	fmt.Println(r.status)
+	if r.status == intialized {
+		if !strings.Contains(string(data), "\r\n") {
+			return 0, nil
+		}
+
+		request := strings.Split(string(data), "\r\n")
+
+		n, err := r.RequestLine.parseRequestLine(request[0])
+
+		if err != nil {
+			return 0, err
+		} else if n == 0 {
+			return 0, nil
+		}
+
+		r.status = done
+	} else if r.status == done {
+		return 0, errors.New("error: trying to read data in a done state")
+	} else {
+
+		return 0, errors.New("error: unkown state")
+	}
+
+	return 0, nil
 }
