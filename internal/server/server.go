f3cbaa2b7f34922d7fa03df2465d9c8e0fc145d7
matteo <matteo.massimello@gmail.com>
2025-03-25 18:11:30 +0100

    feat: done a little

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,12 +1,22 @@
 package server
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net"
 	"strconv"
 
+	"github.com/mattemello/httpFromtcp/internal/request"
 	"github.com/mattemello/httpFromtcp/internal/response"
 )
+
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
 
 type Server struct {
 	Connection net.Listener
@@ -14,7 +24,9 @@ type Server struct {
 	on         bool
 }
 
-func Serve(port int) (*Server, error) {
+func WriteHandlerError(w io.Writer)
+
+func Serve(port int, Hander Handler) (*Server, error) {
 	portString := strconv.Itoa(port)
 	liss, err := net.Listen("tcp", ":"+portString)
 	if err != nil {
@@ -23,7 +35,7 @@ func Serve(port int) (*Server, error) {
 
 	server := &Server{Connection: liss, Port: port, on: false}
 
-	go server.listen()
+	go server.listen(Hander)
 
 	return server, nil
 }
@@ -39,7 +51,7 @@ func (s *Server) Close() error {
 	return nil
 }
 
-func (s *Server) listen() {
+func (s *Server) listen(handler Handler) {
 	log.Println("> Start the listening of the server")
 	for !s.on {
 		conn, err := s.Connection.Accept()
@@ -47,15 +59,25 @@ func (s *Server) listen() {
 			s.Close()
 		}
 
-		go s.handle(conn)
+		go s.handle(conn, handler)
 	}
 }
 
-func (s *Server) handle(conn net.Conn) {
+func (s *Server) handle(conn net.Conn, handler Handler) {
 
-	headers := response.GetDefaultHeaders(0)
-	response.WriteStatusLine(conn, 200)
-	response.WriteHeaders(conn, headers)
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		//todo: respond with an error
+	}
+
+	var buffer bytes.Buffer
+
+	erro := handler(&buffer, req)
+	if erro != nil {
+		//todo: respond with an error
+	}
+
+	defHandler := response.GetDefaultHeaders()
 
 	conn.Close()
 }
